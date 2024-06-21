7ddf9dd72d74aac4b85eebbd8c50fe1154408212
kanchansomani <153606846+kanchansomani@users.noreply.github.com>
2024-06-21 20:02:24 +0530

    created google meet clone

--- src/index.js
+++ src/index.js
@@ -2,24 +2,23 @@ import ReactDom from "react-dom/client";
 import Meetcard from "./components/Meetcard/Meetcard"
 import "./style.css"
 
-import student1 from "./components/Meetcard/image/img1.webp"
-import student2 from "./components/Meetcard/image/img2.jpg"
-import student3 from "./components/Meetcard/image/img3.jpg"
-import student4 from "./components/Meetcard/image/img4.avif"
-import student5 from "./components/Meetcard/image/img5.jpg"
-import student6 from "./components/Meetcard/image/img6.jpg"
-import student7 from "./components/Meetcard/image/img7.jpeg"
-import student8 from "./components/Meetcard/image/img8.jpeg"
-import student9 from "./components/Meetcard/image/img9.avif"
-import student10 from "./components/Meetcard/image/img10.webp"
-import student11 from "./components/Meetcard/image/img11.webp"
-import student12 from "./components/Meetcard/image/img12.avif"
-import student13 from "./components/Meetcard/image/img13.jpg"
-import student14 from "./components/Meetcard/image/img14.jpg"
-import student15 from "./components/Meetcard/image/img15.jpg"
-
+import student1 from "./components/Meetcard/images/img1.jpg"
+import student2 from "./components/Meetcard/images/img2.jpg"
+import student3 from "./components/Meetcard/images/img3.jpeg"
+import student4 from "./components/Meetcard/images/img4.jpeg"
+import student5 from "./components/Meetcard/images/img16.webp"
+import student6 from "./components/Meetcard/images/img5.jpg"
+import student7 from "./components/Meetcard/images/img7.jpeg"
+import student8 from "./components/Meetcard/images/img8.jpeg"
+import student9 from "./components/Meetcard/images/img9.avif"
+import student10 from "./components/Meetcard/images/img10.webp"
+import student11 from "./components/Meetcard/images/img11.webp"
+import student12 from "./components/Meetcard/images/img12.avif"
+import student13 from "./components/Meetcard/images/img13.jpg"
+import student14 from "./components/Meetcard/images/img14.jpg"
+import student15 from "./components/Meetcard/images/img15.jpg"
+import student16 from "./components/Meetcard/images/img17.jpeg"
 import Icons from "./components/Buttons/Buttons";
-
 
 
 const root = ReactDom.createRoot(document.getElementById("root"));
@@ -28,32 +27,32 @@ root.render(<>
 
    <div className="component-container">
       <Meetcard
-         studentName="Mahie"
+         studentName="Kanchan"
          studentImg={student1}
          micState={true} />
 
       <Meetcard
-         studentName="Rajat"
+         studentName="Deepak"
          studentImg={student2}
          micState={false} />
 
       <Meetcard
-         studentName="Mrunmyi"
+         studentName="Archana"
          studentImg={student3}
          micState={false} />
 
       <Meetcard
-         studentName="Atharva"
+         studentName="Dhanshri"
          studentImg={student4}
          micState={false} />
 
       <Meetcard
-         studentName="Raj"
+         studentName="Sanjana"
          studentImg={student5}
          micState={false} />
 
       <Meetcard
-         studentName="Sima"
+         studentName="Divya"
          studentImg={student6}
          micState={true} />
 
@@ -63,32 +62,32 @@ root.render(<>
          micState={true} />
 
       <Meetcard
-         studentName="Ram"
+         studentName="Vishal"
          studentImg={student8}
          micState={false} />
 
       <Meetcard
-         studentName="Vaibhavi"
+         studentName="Rekha"
          studentImg={student9}
          micState={true} />
 
       <Meetcard
-         studentName="Aray"
+         studentName="Gaurav"
          studentImg={student10}
          micState={false} />
 
       <Meetcard
-         studentName="Subodh"
+         studentName="Suraj"
          studentImg={student11}
          micState={true} />
 
       <Meetcard
-         studentName="Saileh"
+         studentName="Sagar"
          studentImg={student12}
          micState={true} />
 
       <Meetcard
-         studentName="Sahil"
+         studentName="Ruturaj"
          studentImg={student13}
          micState={false} />
 
@@ -98,8 +97,13 @@ root.render(<>
          micState={false} />
 
       <Meetcard
-         studentName="Chirag"
+         studentName="Abhi"
          studentImg={student15}
+         micState={true} />
+
+<Meetcard
+         studentName="Abhi"
+         studentImg={student16}
          micState={true} />
 
       <div className="component-icon-container">
